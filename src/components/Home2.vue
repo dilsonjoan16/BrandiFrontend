ca654cec406b360506463684dc5d3c3b14b728b9
dilsonjoan16 <dilsonjoan16@gmail.com>
2021-12-12 00:08:26 -0400

    primera subida del proyecto

---
+++ src/components/Home2.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>
+            Pagina de prueba
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Home2'
+}
+</script>
+<style scoped>
+
+</style>
